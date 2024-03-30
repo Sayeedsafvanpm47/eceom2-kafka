19435674ce0d343bb0ec097275e06877d8f9b547
Sayeed Safvan <sayeedsafvan123@gmail.com>
2024-03-30 11:45:12 +0530

    pushing to test

--- orders2/messagnig/client.js
+++ orders2/messagnig/client.js
@@ -1,5 +1,5 @@
 const {Kafka} = require('kafkajs')
 const kafka = new Kafka({clientId:'ecom-app',brokers:['192.168.1.10:9092']})
-
+console.log('This is client')
 module.exports = {kafka}
 
--- tickets/messagnig/client.js
+++ tickets/messagnig/client.js
@@ -1,4 +1,5 @@
 const {Kafka} = require('kafkajs')
 const kafka = new Kafka({clientId:'ecom-app',brokers:['192.168.1.10:9092']})
+console.log('Client in auth modified')
 module.exports = {kafka}
 
